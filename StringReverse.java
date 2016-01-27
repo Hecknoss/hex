1a56d542e51f5570915a11fbf8d16dba6d4391ab
Hecknoss <admin@hecknoss.com>
2016-01-28 08:58:11 +1100

    Update StringReverse.java

--- StringReverse.java
+++ StringReverse.java
@@ -2,22 +2,26 @@ package learn;
 
 class StringReverse {
 	
-	public static void main(String args[]){
-	
-	String word = "Speghetti";
-	int len = word.length() - 1;
-	System.out.println(word);
-	int i = -1;
-	int c = 0;
-	String word2 = "";
-	while (i < len){
-		char cur = word.charAt(len);
-		word2 += cur;
-		//System.out.println("len = " + len + "\nc = " + c + "\nletter = " + cur + "\n");
-		len -= 1;
-		c += 1;
+	public static String wordReverse(String wordie){
+		String word = wordie;
+		int len = word.length() - 1;
+		System.out.println(word);
+		int i = -1;
+		int c = 0;
+		String word2 = "";
+		while (i < len){
+			char cur = word.charAt(len);
+			word2 += cur;
+			len -= 1;
+			c += 1;
+		}
+		return word2;
 	}
-	System.out.println(word2);
+	
+	
+	public static void main(String args[]){
+		System.out.println(wordReverse("Spegetti"));
+
 	}
 
 }
