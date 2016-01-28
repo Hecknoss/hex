8bfaa500d9b5f5b8fa66202cf499b14e68411b3a
Hecknoss <admin@hecknoss.com>
2016-01-28 21:36:53 +1100

    Create weather_get2.py

---
+++ weather_get2.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+## newbie class practice time.
+
+############# START MODULE #############
+
+class GetPageContent(object):
+    def __init__(self):
+        self.name = None
+
+    def get_page(self, url):
+        try:
+            import urllib
+            return urllib.urlopen(url).read()
+        except:
+            return ''
+
+    def scanner(self, page, tofind, endof):
+        self.page = page
+        self.tofind = tofind
+        self.endof = endof
+
+        self.start = self.page.find(self.tofind)
+        self.end = self.page.find(self.endof, self.start)
+        self.content = self.page[self.start + len(tofind):self.end]
+
+        return self.content
+
+############# END MODULE #############
+
+def get_weather():
+    #create instance
+    link = 'http://www.bom.gov.au/vic/forecasts/melbourne.shtml'
+    weather = GetPageContent()
+    page = weather.get_page(link)
+
+    #define variables to be used
+    str1 = '<h2>'
+    str1end = '</h2>'
+    str2 = '<p>'
+    str2end = '</p>'
+
+    for i in range(7):
+        print weather.scanner(page, str1, str1end)
+        print weather.start
+        page = page[weather.end:]
+        print weather.scanner(page, str2, str2end)
+        i += 1
+
+get_weather()
